0955270a712e3cd6b917f3a0ece63ba378f73107
keyvanatighi <keyvanatighi.acc@gmail.com>
2023-05-13 18:37:01 +0330

    protect routes and auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,6 +66,19 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    get a users
+// @route   /api/users/me
+// @access  private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+
+  res.status(200).json(user);
+});
+
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
@@ -73,4 +86,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
